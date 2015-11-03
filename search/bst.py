7af060068ce66038d5b1fc65d4e75b65eb2e3fc2
Matthew Parrott <mparrott@pixar.com>
2015-11-03 08:55:12 -0800

    delete for bst

--- search/bst.py
+++ search/bst.py
@@ -11,6 +11,9 @@ class Node(object):
         self.left = None
         self.right = None
 
+    def __repr__(self):
+        return '%s=%s' % (self.key, self.value)
+
 
 class BinarySearchTree(object):
 
@@ -31,7 +34,7 @@ class BinarySearchTree(object):
     def _add(self, key, value, node):
         if node is None:
             return Node(key, value)
-        elif node.value >= node.value:
+        elif key >= node.key:
             node.right = self._add(key, value, node.right)
         else:
             node.left = self._add(key, value, node.left)
@@ -53,15 +56,36 @@ class BinarySearchTree(object):
     def get(self, key):
         return self._get(key, self.root)
 
+    def _min(self, node):
+        if node.left is None:
+            return node
+        return self._min(node.left)
+
+    def _delete_min(self, node):
+        if node.left is None:
+            return node.right
+        node.left = self._delete_min(node.left)
+        return node
+
     def _delete(self, key, node):
         if node is None:
             return
         elif key == node.key:
-           pass
+            if node.right is None:
+                return node.left
+            elif node.left is None:
+                return node.right
+            else:
+                x = self._min(node.right)
+                x.right = self._delete_min(node.right)
+                x.left = node.left
+                node = x
+
         elif key >= node.key:
             node.right = self._delete(key, node.right)
         else:
             node.left = self._delete(key, node.left)
+        return node
 
     def delete(self, key):
         self._delete(key, self.root)
@@ -71,6 +95,13 @@ bst = BinarySearchTree()
 bst.add('C', 3)
 bst.add('B', 2)
 bst.add('A', 1)
+bst.add('H', 4)
+bst.add('E', 5)
 print bst
-print bst.get('A')
-print bst.get('B')
+bst.delete('B')
+print bst
+bst.delete('H')
+print bst
+bst.add('Q', 15)
+bst.add('T', 11)
+print bst
