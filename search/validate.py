6d4500fe7e356e6875dd2aba28460c0295325f10
Matthew Parrott <mparrott@pixar.com>
2015-11-04 08:12:56 -0800

    succinct bst validation

--- search/validate.py
+++ search/validate.py
@@ -5,30 +5,24 @@ sys.path.insert(0, os.path.dirname(__file__))
 import bst
 
 
-def inorder(node, flattened):
+def _validate(node):
     if node is None:
-        return
-    else:
-        inorder(node.left, flattened)
-        flattened.append(node.key)
-        inorder(node.right, flattened)
+        return True, None, None
+
+    l_valid, l_min, l_max = _validate(node.left)
+    r_valid, r_min, r_max = _validate(node.right)
+
+    valid = l_valid and r_valid
+    if valid:
+        if l_max is not None:
+            valid = valid and l_max < node.key
+        if r_max is not None:
+            valid = valid and r_max >= node.key
+    return valid, min(r_min, node.key), max(r_max, node.key)
 
 
-def validate_bst(tree):
-    # get the inorder traversal
-    keys = []
-    inorder(tree.root, keys)
-
-    # ensure the inorder values are non-decreasing
-    prev = None
-    for k in keys:
-        if prev is None:
-            prev = k
-        elif k < prev:
-            return False
-        else:
-            prev = k
-    return True
+def validate(tree):
+    return _validate(tree.root)[0]
 
 
 if __name__ == '__main__':
@@ -37,4 +31,13 @@ if __name__ == '__main__':
     tree.add('B', 2)
     tree.add('Q', 3)
     tree.add('A', 2)
-    print validate_bst(tree)
+    print validate(tree)
+
+    # Concoct an invalid tree.
+    tree = bst.BinarySearchTree()
+    tree.root = bst.Node('G', 2)
+    tree.root.left = bst.Node('B', 3)
+    tree.root.left.right = bst.Node('C', 4)
+    tree.root.left.left = bst.Node('X', 5)
+
+    print validate(tree)
