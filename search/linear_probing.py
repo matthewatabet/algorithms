2b674e82d50fd5aebd0568896d1828cf9de1e05f
matthewatabet <matthewatabet@gmail.com>
2015-11-07 14:51:52 -0800

    working linear probe hash

--- search/linear_probing.py
+++ search/linear_probing.py
@@ -28,31 +28,36 @@ class LinearProbingHash(object):
         self.keys = [None] * self.n
         self.values = [None] * self.n
         for key, value in zip(keys, values):
-            self.add(key, value)
+            self._add(key, value)
 
     def resize(self):
         if self.m > self.n / 2:
             self.n *= 2
+            print 'enlarge', self.n, self.m
             self._resize()
         elif self.m < self.n / 4:
+            print 'contract'
             self.n /= 2
             self._resize()
 
-    def add(self, key, value):
+    def _add(self, key, value):
         i = self.hash(key)
-        while self.values[i] is not None:
-            i += 1
+        while self.values[i] is not None and self.keys[i] != key:
+            i = (i + 1) % self.n
         self.keys[i] = key
         self.values[i] = value
+
+    def add(self, key, value):
+        self._add(key, value)
         self.m += 1
         self.resize()
 
     def delete(self, key):
         i = self.hash(key)
         while (self.keys[i] is not None and
-               self.key[i] != key):
+               self.keys[i] != key):
             i += 1
-        if self.key[i] == key:
+        if self.keys[i] == key:
             self.values[i] = None
         self.m -= 1
         self.resize()
@@ -70,9 +75,17 @@ class LinearProbingHash(object):
 if __name__ == '__main__':
     h = LinearProbingHash(1)
     h.add(5, 'a')
+    print h
     h.add(7, 'b')
+    print h
     h.add(2, 'c')
+    print h
     h.add(6, 'd')
+    print h
     h.add(7, 'e')
+    print h
     h.delete(7)
     print h
+    h.delete(6)
+    h.delete(2)
+    h.delete(5)
