83c6cda0c87430b3c66b0483f4d6f4d8bbeb47df
matthewatabet <matthewatabet@gmail.com>
2015-11-14 16:38:23 -0800

    check if two strings are permutations of each other

---
+++ strings/perm.c
@@ -0,0 +1,63 @@
+/*
+ * Check if one string is a permutation of the other.
+ * Use a counter array to keep track of how often characters appear in
+ * each string. Assumes ascii characters.
+ *
+ * Check length of strings.
+ * Initialize two counter arrays.
+ * Count characters in each string via count().
+ * Compare the array values.
+ *     Early return if disimilar.
+ */
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void count(char *str, int *counter, int n)
+{
+	int c;
+	int i;
+	for ( i = 0; i < n; i++)
+	{
+		c = (int) str[i];
+		counter[c] += 1;
+	}
+}
+
+
+int perm(char *a, char *b)
+{
+	size_t a_len = strlen(a);
+	size_t b_len = strlen(b);
+
+	if ( a_len != b_len ) return 0;
+	
+	int a_count[256] = {};
+	int b_count[256] = {}; 
+	
+	count(a, a_count, a_len);
+	count(b, b_count, b_len);
+
+	// compare the results.
+	int i;
+	for ( i = 0; i < 256; i++)
+	{
+		if ( a_count[i] != b_count[i] )
+		{
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
+
+int main()
+{
+	printf("%d\n", perm("this", "siht"));
+	printf("%d\n", perm("abc", "cba"));
+	printf("%d\n", perm("fool", "foo"));
+	printf("%d\n", perm("yop", "fop"));
+	printf("%d\n", perm("this way", "way this"));
+}
