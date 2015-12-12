f4b1b42383f07eed4f3a0b4559cdf179cb63e375
matthewatabet <matthewatabet@gmail.com>
2015-12-12 11:46:50 -0800

    refine for lexagraphical sort if we need to maintain order

--- strings/duplicates.py
+++ strings/duplicates.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python
+'''
+Remove duplicates from input string. But, maintain the ordering of
+characters, and return the least lexagrpahical ordering.
+To do this, store the largest index where each character is found.
+Then, we can accumulate a result string by:
+1) Checking if the last character in the current result can be
+   found later and has a lesser value than the current character,
+   if that's true we can pop the last character.
+2) Add the current character
+'''
 
 
 class Solution(object):
 
     def removeDuplicateLetters(self, s):
-        t = set([])
-        for c in s:
-            t.add(c)
-        t = list(t)
-        t.sort()
-        return t
+        rindex = {c: i for i, c in enumerate(s)}
+        result = ''
+        for i, c in enumerate(s):
+            if c not in result:
+                while c < result[-1:] and i < rindex[result[-1]]:
+                    result = result[:-1]
+                result += c
+        return result
 
 
 def main():
     solution = Solution()
-    print solution.removeDuplicateLetters('gfvbbf')
-    print solution.removeDuplicateLetters('bcabc')
-    print solution.removeDuplicateLetters('acdb')
+    print solution.removeDuplicateLetters('bcabc') == 'abc'
+    print solution.removeDuplicateLetters('cbacdcbc') == 'acdb'
 
 
 if __name__ == '__main__':
